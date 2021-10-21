16379c3a3536e1fc100040fc5cdfdaa1ef7f816f
Gallardo Benjamin <Benjamin.Gallardo1@outlook.fr>
2021-10-21 09:27:20 +0200

    Correction sur le nom du fichier image

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -11,7 +11,7 @@ const storage = multer.diskStorage({
     destination: (req, file, callback) => { // Destination où les fichiers seront enregistrés
         callback(null, 'images'); // 'null' précise qu'il ni a pas d'erreur | 'images' la destination des éléments enregistrés
     },
-    fillename : (req, file, callback) => { // Explication du nom de fichiers à utiliser (On ne veut pas avoir deux fichiers de même nom)
+    filename : (req, file, callback) => { // Explication du nom de fichiers à utiliser (On ne veut pas avoir deux fichiers de même nom)
         const name = file.originalname.split(' ').join('_'); // Récupération du nom original, récupération en tableau des éléments à chaque espace, on lie les éléments avec des underscores
         const extension = MIME_TYPES[file.mimetype]; // Il faut appliquer à une extension du fichiers
         callback(null, name + Date.now() + '.' + extension);
